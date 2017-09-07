a38b59052b1f37672464b68e61e3d8151ce955dc
Sofia Cepeda <sofiaclara93@gmail.com>
2017-09-07 15:13:41 -0400

    find files to copy for emails and products

--- file_copier.rb
+++ file_copier.rb
@@ -3,11 +3,11 @@ require 'fileutils'
 # finds file and copies and changes name
 module FileCopier
   def self.find(filename)
-    path = ""
+    paths = []
     Dir["../**/*#{filename}"].each do |file|
-      path = File.absolute_path(file)
+      paths.push(File.absolute_path(file))
     end
-    return path
+    return paths[0]
   end
 
   def self.copy(filename)
@@ -15,4 +15,40 @@ module FileCopier
     FileUtils.cp find(filename) , "#{Dir.pwd}/emails.csv"
   end
 
+end
+
+
+require 'fileutils'
+
+# finds file and copies and changes name
+module FileCopier
+  def self.find(filename)
+    paths = []
+    Dir["../**/*#{filename}"].each do |file|
+      paths.push(File.absolute_path(file))
+    end
+    if paths.length === 0
+      puts "File not found"
+      # find way to break out of this without error
+    elsif paths.length > 1
+      puts "More than one path exists for this file. Please choose and paste the correct file path:"
+      puts paths
+      path = gets.chomp
+      return path
+    else
+      return paths[0]
+    end
+  end
+
+  def self.email_copy(filename)
+    puts "Thanks 😁"
+    puts "copying file #{filename}..."
+    FileUtils.cp find(filename) , "#{Dir.pwd}/emails.csv"
+  end
+
+  def self.products_copy(filename)
+    puts "Thanks 😁"
+    puts "copying file #{filename}..."
+    FileUtils.cp find(filename) , "#{Dir.pwd}/domains.csv"
+  end
 end
--- runner.rb
+++ runner.rb
@@ -9,16 +9,26 @@ require_relative 'file_copier'
 require 'pry'
 require 'pry-byebug'
 
-# while true
+while true
 
-  puts "Please enter the filename containing the emails or 'quit'"
+  puts "Please enter the filename containing the emails or 'quit':"
 
   emails_answer = gets.chomp
-  # if emails_answer === 'quit'
-  #   break
-  # end
-  #
-  FileCopier.copy(emails_answer)
+  if emails_answer === 'quit'
+    break
+  end
+
+  FileCopier.email_copy(emails_answer)
+
+  puts "Please enter the filename containing the products list or 'quit':"
+
+  products_answer =gets.chomp
+  if products_answer === 'quit'
+    break
+  end
+
+  FileCopier.products_copy(products_answer)
+
 
   # users = UserParser.parse('emails.csv')
   # domains = DomainParser.parse('domains.csv')
@@ -28,4 +38,4 @@ require 'pry-byebug'
   # FileWriter.write(updated_users)
 
 
-# end
+end
